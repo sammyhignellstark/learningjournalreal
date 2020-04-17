ba9bb227c72c5f6972509a006950ea6a3a1c06db
sammyhignellstark <sammyhignellstark@gmail.com>
2020-04-17 14:59:07 -0700

    moved files to journal

---
+++ entranceExam201.js
@@ -0,0 +1,30 @@
+var y = 3 + 4;
+
+var result = 8 * 3;
+
+var x = "Failing is just another word for learning.";
+
+// variable a equals Welcome John Doe;
+
+
+// var greet function name
+//    return "Hello, " + name
+
+// greet()
+
+// var greet function name
+//    return "Hello, " + name
+
+// greet()
+
+// var greet = {
+//     "Hello, " + name;
+//   }
+//   greet();
+
+
+function greet(name) {
+    return "Hello, " + name;
+  }
+  greet("Grace Hopper");
+
